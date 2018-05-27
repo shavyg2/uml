4d56d7d12c6a044f2fff631eec0aa79c9e269144
Test Account <testaccount@Tests-MacBook-Pro.local>
2018-05-26 23:56:11 -0400

    added node shebang

--- bin/Argument.d.ts
+++ bin/Argument.d.ts
@@ -2,6 +2,9 @@ export declare class Application {
     private args;
     constructor(args: string[]);
     private readonly parsed;
-    readonly minimalist: any;
-    private check();
+    readonly minimalist: {
+        watch: boolean;
+        args: string[];
+    };
+    private check(args);
 }
